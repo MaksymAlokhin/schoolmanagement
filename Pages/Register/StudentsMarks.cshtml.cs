dabcf70746ff7dee42e9618d4c77ae315f56efd2
gform <maksym.alokhin@protonmail.com>
2021-08-03 09:52:33 +0300

    Gradebook without filtering by date

--- Pages/Register/StudentsMarks.cshtml.cs
+++ Pages/Register/StudentsMarks.cshtml.cs
@@ -2,15 +2,160 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sms.Models;
 
 namespace sms.Pages.Register
 {
+    [Authorize(Roles = "Адміністратор, Вчитель, Батьки")]
     public class StudentsMarksModel : PageModel
     {
-        public void OnGet()
+        public StudentsMarksModel(sms.Data.ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        private readonly sms.Data.ApplicationDbContext _context;
+        public IList<Gradebook> gradebook { get; set; }
+        public List<SelectListItem> grades;
+        public int selectedGrade;
+        public int selectedSubject;
+        public int selectedMonth;
+        public int selectedYear;
+        public string selectedMark;
+        public int NowYear = DateTime.Now.Year;
+        public List<SelectListItem> SubjectsSL { get; set; }
+        public List<Student> students;
+        public int days;
+        public PaginatedList<int> pages { get; set; }
+        public int? selectedPage;
+
+        public List<SelectListItem> YearSL { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = $"{DateTime.Now.Year-2}", Text = $"{DateTime.Now.Year-2}" },
+            new SelectListItem { Value = $"{DateTime.Now.Year-1}", Text = $"{DateTime.Now.Year-1}" },
+            new SelectListItem { Value = $"{DateTime.Now.Year}", Text = $"{DateTime.Now.Year}" },
+            new SelectListItem { Value = $"{DateTime.Now.Year+1}", Text = $"{DateTime.Now.Year+1}" },
+            new SelectListItem { Value = $"{DateTime.Now.Year+2}", Text = $"{DateTime.Now.Year+2}" }
+        };
+
+
+        //string sMonth = DateTime.Now.ToString("MM"); //01
+        //string sMonth = DateTime.Now.ToString(); //1
+
+        public List<SelectListItem> Months { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Січень" },
+            new SelectListItem { Value = "2", Text = "Лютий" },
+            new SelectListItem { Value = "3", Text = "Березень" },
+            new SelectListItem { Value = "4", Text = "Квітень" },
+            new SelectListItem { Value = "5", Text = "Травень" },
+            new SelectListItem { Value = "6", Text = "Червень" },
+            new SelectListItem { Value = "7", Text = "Липень" },
+            new SelectListItem { Value = "8", Text = "Серпень" },
+            new SelectListItem { Value = "9", Text = "Вересень" },
+            new SelectListItem { Value = "10", Text = "Жовтень" },
+            new SelectListItem { Value = "11", Text = "Листопад" },
+            new SelectListItem { Value = "12", Text = "Грудень" }
+        };
+
+        public async Task OnGetAsync(int? pageIndex, int gradeId = 0, int subjectId = 0, int year = 0, int month = 9)
+        {
+            selectedGrade = gradeId;
+            selectedSubject = subjectId;
+            selectedMonth = month;
+            selectedPage = pageIndex;
+            if (year == 0) selectedYear = DateTime.Now.Year;
+            else selectedYear = year;
+
+            if (gradeId != 0)
+            {
+                SubjectsSL = _context.Curricula
+                    .AsNoTracking()
+                    .Include(g => g.Subject)
+                    .Where(g => g.GradeId == gradeId)
+                    .OrderBy(g => g.Subject.Name)
+                    .Select(g =>
+                    new SelectListItem
+                    {
+                        Value = g.SubjectId.ToString(),
+                        Text = g.Subject.Name
+                    }).ToList();
+            }
+            else
+            {
+                SubjectsSL = new List<SelectListItem>()
+                    {
+                        new SelectListItem
+                        {
+                            Value = null,
+                            Text = " "
+                        }
+                    };
+
+            }
+            if (month != 0 && year != 0)
+            {
+                days = DateTime.DaysInMonth(selectedYear, month);
+            }
+            else { days = 31; }
+
+            var slots = Enumerable.Range(1, days).ToList();
+            var pageSize = 11;
+            pages = PaginatedList<int>.CreateFromList(slots, pageIndex ?? 1, pageSize);
+
+
+            if (gradeId != 0)
+            {
+                students = await _context.Students
+                    .Where(g => g.GradeId == gradeId)
+                    .OrderBy(g => g.LastName)
+                    .ThenBy(g => g.FirstName)
+                    .ToListAsync();
+            }
+            else { students = new List<Student>(); }
+
+            grades = new List<SelectListItem>();
+            var grad = _context.Grades.OrderBy(g => g.Number).ThenBy(g => g.Letter);
+            foreach (Grade g in grad)
+            {
+                grades.Add(new SelectListItem { Value = $"{g.Id}", Text = $"{g.FullName}" });
+            }
+
+            gradebook = await _context.Gradebooks
+                .Include(g => g.Student)
+                .Include(g => g.Subject)
+                .Include(g => g.Teacher)
+                .Where(g => g.LessonDate.Month == month && g.LessonDate.Year == year && g.SubjectId == subjectId && g.Student.GradeId == gradeId)
+                .ToListAsync();
+
+        }
+        public JsonResult OnGetSubjects(string gradeId)
+        {
+            if (!string.IsNullOrWhiteSpace(gradeId))
+            {
+
+                IEnumerable<SelectListItem> subjects = _context.Curricula
+                    .AsNoTracking()
+                    .Include(g => g.Subject)
+                    .Where(g => g.GradeId == int.Parse(gradeId))
+                    .OrderBy(g => g.Subject.Name)
+                    .Select(g =>
+                    new SelectListItem
+                    {
+                        Value = g.SubjectId.ToString(),
+                        Text = g.Subject.Name
+                    }).ToList();
+
+                //var subj = new SelectList(subjects, "Value", "Text");
+
+                return new JsonResult(subjects);
+            }
+            return null;
         }
     }
 }
